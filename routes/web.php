5588cc3db1f49d18100c06c90e0acfa4e3adcb2b
xachman <zironside@hotmail.com>
2026-09-05 18:15:22 +0000

    add logout.

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,13 @@ Route::view('/', 'welcome');
 
 // Livewire routes
 Route::get('/login', \App\Livewire\Login::class)->name('login');
+Route::post('/logout', function () {
+    Auth::logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+
+    return redirect()->route('login');
+})->name('logout');
 
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::view('/admin', 'livewire.admin')->name('admin');
